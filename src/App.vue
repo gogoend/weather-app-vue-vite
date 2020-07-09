9169124f840cbc6251c3a7c3441e0592d91a8461
gogoend <gogoend@qq.com>
2020-07-09 20:29:47 +0800

    enhance: init

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<!--
+ * @Author: gogoend
+ * @Date: 2020-07-07 22:06:19
+ * @LastEditors: gogoend
+ * @LastEditTime: 2020-07-07 22:13:06
+ * @FilePath: \vite-test\src\App.vue
+ * @Description: 
+--> 
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld msg="Hello Vue 3.0 + Vite" />
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
